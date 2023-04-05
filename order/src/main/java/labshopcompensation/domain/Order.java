ffd4d3fe89c40c3a321712f44ebaa33430b18d50
TOMKDS <114798900+TOMKDS@users.noreply.github.com>
2023-04-05 13:35:53 +0900

    commit

---
+++ order/src/main/java/labshopcompensation/domain/Order.java
@@ -0,0 +1,62 @@
+package labshopcompensation.domain;
+
+import java.util.Date;
+import java.util.List;
+import javax.persistence.*;
+import labshopcompensation.OrderApplication;
+import labshopcompensation.domain.OrderCancelled;
+import labshopcompensation.domain.OrderPlaced;
+import lombok.Data;
+
+@Entity
+@Table(name = "Order_table")
+@Data
+public class Order {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String productId;
+
+    private Integer qty;
+
+    private String customerId;
+
+    private Double amount;
+
+    private String status;
+
+    private String address;
+
+    @PostPersist
+    public void onPostPersist() {
+        OrderPlaced orderPlaced = new OrderPlaced(this);
+        orderPlaced.publishAfterCommit();
+    }
+
+    @PostRemove
+    public void onPostRemove() {
+        OrderCancelled orderCancelled = new OrderCancelled(this);
+        orderCancelled.publishAfterCommit();
+    }
+
+    @PrePersist
+    public void onPrePersist() {
+        // Get request from Order
+        //labshopcompensation.external.Order order =
+        //    Application.applicationContext.getBean(labshopcompensation.external.OrderService.class)
+        //    .getOrder(/** mapping value needed */);
+
+    }
+
+    @PreRemove
+    public void onPreRemove() {}
+
+    public static OrderRepository repository() {
+        OrderRepository orderRepository = OrderApplication.applicationContext.getBean(
+            OrderRepository.class
+        );
+        return orderRepository;
+    }
+}
